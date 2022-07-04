635184c316adbcef64a26045b5712a861c56beda
Yura <merda@tut.by>
2022-07-05 02:26:37 +0300

    site express

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
-const app = express()
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
 
 const PORT = process.env.PORT || 3000
 
