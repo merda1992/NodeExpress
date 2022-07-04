be5063908553cc62bf34367202cd1412f997e251
Yura <merda@tut.by>
2022-07-05 02:48:53 +0300

    3

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const path = require('path');
+const exphs = require('express-handlebars');
+const { extname } = require('path');
+
 const app = express();
+
+//настройка хэндлбарс
+const hbs = exphs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+
+})
+
+//движок для рендеринга хтмла страниц
+app.engine('hbs', hbs.engine)
+//используем этот движок
+app.set('view engine', 'hbs')
+//хранение шаблонов
+app.set('views', 'views')
 
 app.get('/', (req, res) => {
     res.status(200)
